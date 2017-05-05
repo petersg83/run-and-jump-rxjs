a7a65e29d3436d80c3c96351b3076c2adf61162f
Pierre Noël <pierre.noel.external@sigfox.com>
2017-05-05 17:18:59 +0200

    adapt text and layout

--- src/js/model/game-over-text.js
+++ src/js/model/game-over-text.js
@@ -2,15 +2,15 @@ import { Text } from './text';
 
 export class GameOverText extends Text {
     constructor(canvas, scale) {
-        super(canvas, 11 * scale);
-        this.x = 25 * scale;
+        super(canvas, 10 * scale);
+        this.x = 25 * scale + 60;
         this.y = canvas.height / 2;
         this.visible = false;
     }
 
     render() {
         if (this.visible) {
-            super.render('Game Over! Press Enter to Retry');
+            super.render('Pôle emploi a eu Mlle Pillon!');
         }
     }
 
